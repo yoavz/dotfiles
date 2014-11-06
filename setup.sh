68b8a655ebfdb381015601110dbae9cddaf82084
Yoav Zimmerman <yoavzimmerman@gmail.com>
2014-11-06 12:53:27 +0100

    switch to zsh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ mv ~/.vim ~/.old_vim
 ln -sfn ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+# ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
